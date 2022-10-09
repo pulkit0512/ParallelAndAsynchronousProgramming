206d8c940cc3654a768ebf53ee67cdab86ec54a4
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-10 02:23:59 +0530

    Executor Service

--- ReadMe.md
+++ ReadMe.md
@@ -36,4 +36,34 @@ execute and when. Whereas in case Multi Core, Threads work simultaneously.**
 - Low level
 - Easy to introduce complexity in to our code.
 - Threads are expensive 
-- Threads have their own runtime-stack, memory, registers and more
+- Threads have their own runtime-stack, memory, registers and more. This is why creating and destroying the threads are expensive operations.
+
+## Thread Pool
+- Thread Pool is a group of threads created and readily available
+- CPU Intensive Tasks
+- ThreadPool Size = No of Cores, because threads will be busy performing the CPU intensive tasks.
+- I/O task
+- ThreadPool Size > No of Cores, because most of the time, threads will be waiting to get the data from downstream.
+- What are the benefits of thread pool?
+- No need to manually create, start and join the threads
+- Achieving Concurrency in your application
+
+## ExecutorService
+- Released as part of Java5
+- ExecutorService in Java is an **Asynchronous Task Execution Engine**
+- It provides a way to asynchronously execute tasks and provides the results in a much simpler way compared to thread
+- This enabled coarse-grained task based parallelism in Java
+
+### Working of an Executor Service
+- Executor service has a thread pool, which is a group of threads readily available to execute tasks.
+- It also has a Work Queue and Completion Queue.
+- Work Queue acts as a blocking queue. All the tasks are placed in the work queue for the thread present in thread pool to pick and execute.
+- This returns a Future which is something we can think is like a proxy for which we will get result in later.
+- Once the thread completes the execution it places the result in completion queue, and we can get result from this queue using the Future.
+
+![img.png](ExecutorServiceWorking.png)
+
+### Limitations of ExecutorService
+- Designed to Block the Thread
+- No better way to combine futures
+
---
+++ src/main/java/com/learnjava/executor/ProductServiceUsingExecutor.java
@@ -0,0 +1,70 @@
+package com.learnjava.executor;
+
+import com.learnjava.domain.ecommerce.Product;
+import com.learnjava.domain.ecommerce.ProductInfo;
+import com.learnjava.domain.ecommerce.Review;
+import com.learnjava.productservice.ProductInfoService;
+import com.learnjava.productservice.ReviewService;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static com.learnjava.util.CommonUtil.stopWatch;
+import static com.learnjava.util.LoggerUtil.log;
+
+public class ProductServiceUsingExecutor {
+    private ProductInfoService productInfoService;
+    private ReviewService reviewService;
+
+    // Creates an executor service with threads equals to the number of cores in the machine.
+    static ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        try {
+            ProductServiceUsingExecutor productService = new ProductServiceUsingExecutor();
+            productService.setProductInfoService(new ProductInfoService());
+            productService.setReviewService(new ReviewService());
+
+            String productId = "ABC123";
+            Product product = productService.retrieveProductDetails(productId);
+            log("Product is: " + product);
+        } catch(TimeoutException e) {
+            log("Timeout Exception: " + e.getMessage());
+        } finally {
+            // Explicitly we need to shut down the executor service.
+            // Else it will keep waiting for the new tasks and will be up and running.
+            executorService.shutdown();
+        }
+    }
+
+    public Product retrieveProductDetails(String productId) throws ExecutionException, InterruptedException, TimeoutException {
+        stopWatch.start();
+
+        Future<ProductInfo> productInfoFuture = executorService.submit(() -> productInfoService.getProductInfo(productId));
+        Future<Review> reviewFuture = executorService.submit(() -> reviewService.retrieveReviews(productId));
+
+        // We can give a timeout as well to the future, if we get result in this time then good else throw a timeout exception for the service.
+        // Issue: Blocks the caller thread until we get result from future. We can time out but still it blocks the caller thread for some time.
+        // Futures are designed to block the threads.
+        // No better way to combine the futures.
+        ProductInfo productInfo = productInfoFuture.get(2, TimeUnit.SECONDS);
+        Review review = reviewFuture.get();
+
+        stopWatch.stop();
+        log("Total Time Taken : "+ stopWatch.getTime());
+        return new Product(productId, productInfo, review);
+    }
+
+
+    public void setProductInfoService(ProductInfoService productInfoService) {
+        this.productInfoService = productInfoService;
+    }
+
+    public void setReviewService(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+}
